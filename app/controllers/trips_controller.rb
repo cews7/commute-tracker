ad83e138d8577eac2ea95ebfa266278637cf281a
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-04-17 18:46:59 -0600

    make form for new commute

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,18 @@ class TripsController < ApplicationController
   def show
     @commute = Trip.find(params[:id])
   end
+
+  def new
+    @commute = Trip.new
+  end
+
+  def create
+    @commute = Trip.new(trip_params)
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:longitude, :latitude, :address_one, :address_two, :commute_name)
+  end
 end
